7160b9a64ec9128c87139ee5bcaf6bcf94c526e1
Angga1453 <anggadwip.493@gmail.com>
2024-02-27 15:58:31 +0700

    first commit

--- LoginSystem.java
+++ LoginSystem.java
@@ -1,19 +1,15 @@
-import java.util.Scanner;
-
+import java.util.Scanner; 
 class LibrarySystem {
     private final String adminUsername = "admin3";
     private final String adminPassword = "admin5";
     private final String mahasiswaNIM = "202310370311347";
-
     public boolean loginAdmin(String username, String password) {
         return username.equals(adminUsername) && password.equals(adminPassword);
     }
-
     public boolean loginMahasiswa(String nim) {
         return nim.length() == 15 && nim.equals(mahasiswaNIM);
     }
 }
-
 public class LoginSystem {
     public static void main(String[] args) {
         LibrarySystem librarySystem = new LibrarySystem();
@@ -44,7 +40,6 @@ public class LoginSystem {
             }
         } while (choice != 3);
     }
-
     private static void loginAdmin(LibrarySystem librarySystem, Scanner scanner) {
         System.out.print("Masukkan username: ");
         String adminUsername = scanner.next();
@@ -57,7 +52,6 @@ public class LoginSystem {
             System.out.println("Login gagal");
         }
     }
-
     private static void loginMahasiswa(LibrarySystem librarySystem, Scanner scanner) {
         System.out.print("Masukkan NIM: ");
         String nim = scanner.next();
